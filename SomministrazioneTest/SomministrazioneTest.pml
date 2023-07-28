e07f8147cee0d9228700295feef82e7ae3b47e83
Gianmarco Donnesi <91615472+GianmarcoDonnesi@users.noreply.github.com>
2023-07-28 09:30:38 +0200

    Add files via upload

---
+++ SomministrazioneTest/SomministrazioneTest.pml
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="TestDiscriminazioneUditiva" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="Test" src="Test/Test.dlg" />
+    </Dialogs>
+    <Resources>
+        <File name="TOL1" src="html/TOL1.png" />
+        <File name="TOL10" src="html/TOL10.png" />
+        <File name="TOL11" src="html/TOL11.png" />
+        <File name="TOL12" src="html/TOL12.png" />
+        <File name="TOL13" src="html/TOL13.png" />
+        <File name="TOL2" src="html/TOL2.png" />
+        <File name="TOL3" src="html/TOL3.png" />
+        <File name="TOL4" src="html/TOL4.png" />
+        <File name="TOL5" src="html/TOL5.png" />
+        <File name="TOL6" src="html/TOL6.png" />
+        <File name="TOL7" src="html/TOL7.png" />
+        <File name="TOL8" src="html/TOL8.png" />
+        <File name="TOL9" src="html/TOL9.png" />
+        <File name="choice_sentences" src="behavior_1/Aldebaran/choice_sentences.xml" />
+        <File name="attesa" src="html/attesa.png" />
+        <File name="Score" src="Score.txt" />
+        <File name="TOL_time" src="TOL_time.txt" />
+        <File name="swiftswords_ext" src="behavior_1/swiftswords_ext.mp3" />
+        <File name="taichimove" src="behavior_1/taichimove.pmt" />
+        <File name="fine" src="html/fine.png" />
+    </Resources>
+    <Topics>
+        <Topic name="Test_enu" src="Test/Test_enu.top" topicName="Test" language="it_IT" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="it_IT">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+        <Translation name="translation_it_IT" src="translations/translation_it_IT.ts" language="it_IT" />
+    </Translations>
+</Package>
---
+++ SomministrazioneTest/translations/translation_it_IT.ts
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Fine test</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Complimenti!, hai terminato il test. A presto!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Complimenti!, hai terminato il test. A presto!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (10)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (8)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Figure/Preparati (9)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TestTowerofLondon/Preparati</name>
+        <message>
+            <source>Prepàrati per la prossima figura!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Prepàrati per la prossima figura!</translation>
+        </message>
+    </context>
+</TS>
